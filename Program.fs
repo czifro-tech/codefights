a22486ce866bf3bf686a286ef532b0844941f7e3
Will Czifro <will@czifro.com>
2019-08-14 15:49:49 -0700

    Update to .NET Core 2.1 and simplify module selection

--- Program.fs
+++ Program.fs
@@ -1,33 +1,43 @@
-﻿namespace CodeFightsProblem
+﻿namespace CodeFights
 
 open System
 
   module Main =
 
-    let modules : Map<string,(unit->unit)> =
+    let modules : (string * (unit->unit)) list =
       [
         "InterviewPractice", InterviewPractice.SimulatorHelper.chooseSimulator
+        "Quit", id
       ]
-      |> Map.ofList
 
-    let chooseModule() =
-      let mutable validChoice = false
-      while not validChoice do
-        printfn "Please choose a module: "
-        modules
-        |> Map.iter(fun k _ ->
-          printfn "%s" k
-        )
-        printf "|> "
-        let choice = System.Console.ReadLine()
-        let modOp = Map.tryFind choice modules
-        if modOp.IsNone then
-          printfn "Please be exact!"
-        else
-          validChoice <- true
-          modOp.Value()
+    let rec chooseModule() =
+      printfn "Please choose a module: "
+      modules
+      |> List.iteri(fun i s -> printfn "    %d. %s" (i+1) (fst s))
+      printf "|> "
+      let choice = Console.ReadLine()
+      let result =
+        match Int32.TryParse choice with
+        | true, choice ->
+          modules
+          |> List.tryItem (choice-1)
+          |> function
+          | Some (_, mod')   -> mod'(); Some()
+          | None            -> None
+        | _, _ -> None
+      result
+      |> Option.orElseWith(fun _ ->
+        match modules |> List.tryFind (fst >> (=) choice) with
+        | Some (_, mod') -> mod'(); Some()
+        | None          -> None
+      )
+      |> function
+      | Some _ -> ()
+      | _ ->
+        printfn "Invalid option: %s" choice
+        chooseModule();
 
     [<EntryPoint>]
-    let main argv = 
+    let main argv =
       chooseModule()
       0 // return an integer exit code
